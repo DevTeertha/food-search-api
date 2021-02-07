6b62fac78c1852acbb314abae62cfe619d94bef6
Dev Teertha <devteertha28@gmail.com>
2021-02-07 08:03:05 -0600

    final

--- app.js
+++ app.js
@@ -1,44 +1,5 @@
-// fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-//     .then(res => res.json())
-//     .then(data => displayFirst(data.categories))
-
-// const displayFirst = data => {
-//     data.forEach(element => {
-//         const container = document.getElementById('search-container');
-//         const foodDiv = document.createElement('div');
-//         foodDiv.className = 'food';
-//         const foodInfo = `
-//             <img src="${element.strCategoryThumb}">
-//             <h1>${element.strCategory}</h1>
-//             <button for='food-item' onclick="showDetails('${element.strCategory}')" >Details</button>`;
-
-//         foodDiv.innerHTML = foodInfo;
-//         container.appendChild(foodDiv);
-
-//         const foodItem = document.getElementsByClassName('food');
-//     });
-// }
-
-// const showDetails = details => {
-//     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${details}`;
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data);
-//     })
-// }
-
-
-
-const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=Chicken`;
-fetch(url)
-    .then(res => res.json())
-    .then(data => displayFood(data))
-
-
 document.getElementById('search-btn').addEventListener('click', function () {
     const searchInput = document.getElementById('search-box').value;
-
     if (searchInput === "") {
         alert('Search Box Is Empty!');
     }
@@ -52,7 +13,9 @@ document.getElementById('search-btn').addEventListener('click', function () {
     }
 });
 
+
 const getDataFetch = url =>{
+    document.getElementById('show-details').style.display = 'none';
     fetch(url)
             .then(res => res.json())
             .then(data => displayFood(data))
@@ -91,17 +54,18 @@ const showDetails = details => {
     let container = document.getElementById('show-details');
     const detailsDiv = document.createElement('div');
     detailsDiv.className = 'details';
+    container.style.display = "block";
     container.innerHTML = "";
     const detailsInfo = `
         <img src="${details[0].strMealThumb}">
         <h4>${details[0].strMeal}</h4>
         <h6>Ingredients: </h6>
-        <p class = "list-item">${details[0].strIngredient1 + ' ' + details[0].strMeasure1}</p>
-        <p class = "list-item">${details[0].strIngredient2 + ' ' + details[0].strMeasure2}</p>
-        <p class = "list-item">${details[0].strIngredient3 + ' ' + details[0].strMeasure3}</p>
-        <p class = "list-item">${details[0].strIngredient4 + ' ' + details[0].strMeasure4}</p>
-        <p class = "list-item">${details[0].strIngredient5 + ' ' + details[0].strMeasure5}</p>
-        <p class = "list-item">${details[0].strIngredient6 + ' ' + details[0].strMeasure6}</p>
+        <p class = "list-item">-> ${details[0].strIngredient1 + ' ' + details[0].strMeasure1}</p>
+        <p class = "list-item">-> ${details[0].strIngredient2 + ' ' + details[0].strMeasure2}</p>
+        <p class = "list-item">-> ${details[0].strIngredient3 + ' ' + details[0].strMeasure3}</p>
+        <p class = "list-item">-> ${details[0].strIngredient4 + ' ' + details[0].strMeasure4}</p>
+        <p class = "list-item">-> ${details[0].strIngredient5 + ' ' + details[0].strMeasure5}</p>
+        <p class = "list-item">-> ${details[0].strIngredient6 + ' ' + details[0].strMeasure6}</p>
     `;
 
     detailsDiv.innerHTML = detailsInfo;
